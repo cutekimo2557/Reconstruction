7a0b4d1d6632138d8440049d5d4bc735098f481b
GYUSEOK LEE <148178025+cutekimo2557@users.noreply.github.com>
2024-06-19 19:51:12 +0900

    Create contact_error.h

---
+++ contact_error.h
@@ -0,0 +1,13 @@
+#pragma once
+#ifndef CONTACT_H
+#define CONTACT_H
+
+#include <Windows.h>
+#include <iostream>
+#include <string>
+
+std::string GetLastErrorAsString();
+void ShowErrorMessageBox(const std::string&);
+
+
+#endif
